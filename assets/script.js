bce42feb9f33c54f44ef1a565bcdb0a5e66905ad
Aurelien <oc@aurelienantonio.com>
2022-05-22 20:20:51 -0500

    Fix issues

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,18 @@
 const slides = [
 	{
-		"image":"images/slideshow/slide1.jpg",
-		"tagLine":"<p>Impressions tous formats <span>en boutique et en ligne</span></p>"
+		"image":"slide1.jpg",
+		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
 	},
 	{
-		"image":"images/slideshow/slide2.jpg",
-		"tagLine":"<p>Tirages haute définition grand format <span>pour vos bureaux et events</span></p>"
+		"image":"slide2.jpg",
+		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
 	},
 	{
-		"image":"images/slideshow/slide3.jpg",
-		"tagLine":"<p>Grand choix de couleurs <span>de CMJN aux pantones</span></p>"
+		"image":"slide3.jpg",
+		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
 	},
 	{
-		"image":"images/slideshow/slide3.jpg",
-		"tagLine":"<p>Autocollants <span>avec découpe laser sur mesure</span></p>"
+		"image":"slide4.png",
+		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
