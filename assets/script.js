134ed9d32ba3a0f44acb556e6727f78e09676568
Alexandre LACOUR <100684188+Akalabete@users.noreply.github.com>
2023-04-26 17:02:40 +0200

    mise en place et premier commit

--- assets/script.js
+++ assets/script.js
@@ -16,3 +16,77 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+// initialisation de la bannierere
+let activeBubble = 0;
+let newActiveBubble = 0;
+
+
+const dots = document.querySelector(".dots")
+let table = []
+for (let i = 0; i < slides.length; i++){
+	const imageElement = document.createElement("img");  
+	imageElement.className = "dot";  
+	dots.appendChild(imageElement)
+	table.push(`./assets/images/slideshow/`+slides[i].image)
+}
+updateActiveBubble();
+
+function updateActiveBubble(){
+	const dots = document.querySelectorAll(".dot");
+	dots.forEach(function(item, index){
+		if(index === activeBubble){
+			item.classList.add("active");
+		}else {
+			item.classList.remove("active");
+		}
+	})
+}
+
+afficherNewPic();
+
+function afficherNewPic(){
+	document.querySelector(".banner-img").innerHTML=`<img src="`+ table[activeBubble] + `" />`
+}
+
+// ajout des flèches et du message d'alerte au click
+
+const leftArrowClick = document.querySelector(".arrow_left");
+leftArrowClick.addEventListener("click", function(){
+	//return alert("click gauchet");
+	leftClick();
+});
+
+
+function leftClick(){ 
+	if(activeBubble === 0){
+		newActiveBubble = 3;
+	}else {
+		newActiveBubble = activeBubble -1;
+	}
+	activeBubble = newActiveBubble;
+	afficherNewPic();
+	updateActiveBubble();
+}
+
+
+const rightArrowClick = document.querySelector(".arrow_right");
+rightArrowClick.addEventListener("click",function(){
+	//return alert("click droit");
+	rightClick();
+});
+
+function rightClick(){ 
+	if(activeBubble === 3){
+		newActiveBubble = 0;
+	}else {
+		newActiveBubble = activeBubble + 1;
+	}
+	activeBubble = newActiveBubble;
+	afficherNewPic();
+	updateActiveBubble();
+}
+
+
+
+
+
