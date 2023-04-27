d55d3fda131ea16e0cac8b83e40ecb121c4f6111
Alexandre LACOUR <100684188+Akalabete@users.noreply.github.com>
2023-04-27 11:53:23 +0200

    debugging et refactorisation, projet bouclé

--- assets/script.js
+++ assets/script.js
@@ -16,64 +16,63 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
-// initialisation de la bannierere
+// initialisation des variables
 let activeBubble = 0;
 let newActiveBubble = 0;
-
+let urlListe = []
 
 const dots = document.querySelector(".dots")
-let table = []
+const bannerImg = document.querySelector(".banner-img");
+//création des bulletpoints 
 for (let i = 0; i < slides.length; i++){
 	const imageElement = document.createElement("img");  
 	imageElement.className = "dot";  
 	dots.appendChild(imageElement)
-	table.push(`./assets/images/slideshow/`+slides[i].image)
+	urlListe.push(`./assets/images/slideshow/`+slides[i].image) // j'utilise la boucle pour implémenter une table d'url
 }
+
+//mise en place de la bubble avec class dot_selected
 updateActiveBubble();
 
 function updateActiveBubble(){
-	const dots = document.querySelectorAll(".dot");
-	dots.forEach(function(item, index){
+	const dotsCount = document.querySelectorAll(".dot");
+	dotsCount.forEach(function(item, index){
 		if(index === activeBubble){
-			item.classList.add("active");
+			item.classList.add("dot_selected");
 		}else {
-			item.classList.remove("active");
+			item.classList.remove("dot_selected");
 		}
 	})
 }
-
+//fonction qui affiche l'image du slider indexee sur la bubble active.
 afficherNewPic();
 
 function afficherNewPic(){
-	document.querySelector(".banner-img").innerHTML=`<img src="`+ table[activeBubble] + `" />`
+	bannerImg.src = urlListe[activeBubble]
+
 }
 
-// ajout des flèches et du message d'alerte au click
+// ajout des flèches et du message d'alerte au click puis fonctionnalité
 
 const leftArrowClick = document.querySelector(".arrow_left");
 leftArrowClick.addEventListener("click", function(){
 	//return alert("click gauchet");
 	leftClick();
 });
-
-
+const rightArrowClick = document.querySelector(".arrow_right");
+rightArrowClick.addEventListener("click",function(){
+	//return alert("click droit");
+	rightClick();
+});
+//fonctions au clique
 function leftClick(){ 
 	if(activeBubble === 0){
 		newActiveBubble = 3;
 	}else {
 		newActiveBubble = activeBubble -1;
 	}
-	activeBubble = newActiveBubble;
-	afficherNewPic();
-	updateActiveBubble();
+	updateAfterClick ()
 }
-
-
-const rightArrowClick = document.querySelector(".arrow_right");
-rightArrowClick.addEventListener("click",function(){
-	//return alert("click droit");
-	rightClick();
-});
 
 function rightClick(){ 
 	if(activeBubble === 3){
@@ -81,12 +80,14 @@ function rightClick(){
 	}else {
 		newActiveBubble = activeBubble + 1;
 	}
+	updateAfterClick ()
+}
+
+//refactorisation des fonctions au clique
+
+function updateAfterClick (){
 	activeBubble = newActiveBubble;
 	afficherNewPic();
 	updateActiveBubble();
 }
-
-
-
-
 
