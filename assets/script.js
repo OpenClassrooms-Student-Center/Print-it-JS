a3cc2c5e964c855a0c20dd44a2f6296d9338a234
David Andaman <david.andaman@gmail.com>
2023-09-27 15:31:26 +0200

    Fichier JS - event listener et console log : ok

--- assets/script.js
+++ assets/script.js
@@ -16,3 +16,38 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+
+//console.log(slides[0])
+//console.log(slides[1])
+//console.log(slides[2])
+//console.log(slides[3])
+
+//let i = 0
+
+
+//let btnNext = document.querySelector(".arrow_left")
+//let btnPrevious = document.querySelector("arrow_right")
+//afficherImage(slides[i])
+//btnNext.addEventListener("click", () => {
+	//console.log(i++)
+//})
+
+
+
+//let footer = document.querySelector("footer")
+//footer.innerHTML = div
+
+console.log(slides[0])
+console.log(slides[1])
+console.log(slides[2])
+console.log(slides[3])
+
+let slideBoutonLeft = document.querySelector(".arrow_left");
+slideBoutonLeft.addEventListener("click", function () {
+	console.log("Vous avez cliqué sur le bouton gauche")
+});
+
+let slideBoutonRight = document.querySelector(".arrow_right");
+slideBoutonRight.addEventListener("click", function () {
+	console.log("Vous avez cliqué sur le bouton droite")
+});
