c0d6372f88a0bb60c54f7d76b5491c55da27bceb
stephanieEt <stetourneau@laposte.net>
2023-12-20 12:22:12 +0100

    change slide and add condition

--- assets/script.js
+++ assets/script.js
@@ -20,26 +20,68 @@ const slides = [
 
 /*Ajout des Event Listeners sur les flèches*/
 
-const arrowleft = document.querySelector(".arrow_left");
-function showMsg() {
+let arrowleft = document.querySelector(".arrow_left");
+function slideLeft() {
   console.log("Vous avez cliqué sur la flèche de gauche");
 }
-arrowleft.addEventListener("click", showMsg);
+arrowleft.addEventListener("click", slideLeft);
 
-const arrowright = document.querySelector(".arrow_right");
-function showMsg() {
+let arrowright = document.querySelector(".arrow_right");
+function slideRight() {
   console.log("Vous avez cliqué sur la flèche de droite");
 }
-arrowright.addEventListener("click", showMsg);
+arrowright.addEventListener("click", slideRight);
 
 /*Création des bulles points*/
+
 let dotsBullet = document.querySelector(".dots");
 let i = 0;
+
 for (let i = 0; i < slides.length; i++) {
   const dot = document.createElement("div");
   dot.className = "dot";
-
   dotsBullet.appendChild(dot);
 }
+
 let dotsCheck = document.querySelectorAll(".dot");
 dotsCheck[i].classList.add("dot_selected");
+
+/*Changement de slide au clic sur la flèche droite*/
+
+const bannerImage = document.querySelector(".banner-img");
+
+const bannerText = document.querySelector("#banner p");
+
+function slideRight() {
+  dotsCheck[i].classList.remove("dot_selected");
+  /*i += 1; lors de létape 4, puis condition pour avoir le défilement étape 5*/
+
+  if (i >= slides.length - 1) {
+    i = 0;
+  } else {
+    i++;
+  }
+
+  dotsCheck[i].classList.add("dot_selected");
+
+  /*changement de l'image et du titre*/
+  bannerImage.src = `./assets/images/slideshow/${slides[i].image}`;
+  bannerText.innerHTML = slides[i].tagLine;
+}
+
+function slideLeft() {
+  dotsCheck[i].classList.remove("dot_selected");
+  /*i -= 1; lors de l'étape 4, puis condition pour avoir le défilement étape 5*/
+
+  if (i <= 0) {
+    i = slides.length - 1;
+  } else {
+    i--;
+  }
+
+  dotsCheck[i].classList.add("dot_selected");
+
+  /*changement de l'image et du titre*/
+  bannerImage.src = `./assets/images/slideshow/${slides[i].image}`;
+  bannerText.innerHTML = slides[i].tagLine;
+}
