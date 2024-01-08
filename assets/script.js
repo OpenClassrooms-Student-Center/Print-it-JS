2063eb6dda2cb28d4aa1588269aad4b819f8853f
Basmahx <138460878+Basmahx@users.noreply.github.com>
2024-01-08 05:50:49 +0100

    les dots et event listeners sont ajouté

--- assets/script.js
+++ assets/script.js
@@ -15,4 +15,48 @@ const slides = [
 		"image":"slide4.png",
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
-]
+];
+
+
+// Récuperer les elements :)
+
+const ImagesSlider=document.getElementById("js-banner-img");
+console.log(ImagesSlider) 
+
+const DotsSlider=document.getElementById("js-dots"); // pour créé les dots
+console.log(DotsSlider)
+
+// les flèches
+const ArrowLeft = document.getElementById("js-arrow_left"); 
+const ArrowRight = document.getElementById("js-arrow_right"); 
+
+
+
+
+
+// ajouter les dots
+
+for (let i = 0; i < 4; i++) {
+    const newDot = document.createElement("div");
+    newDot.classList.add("dot");
+    DotsSlider.appendChild(newDot);
+}
+
+// Set the first dot as active initially
+DotsSlider.children[0].classList.add("active");
+
+
+
+
+// ajouter les event listeners à des flèches
+
+ArrowLeft.addEventListener('click', function() {
+    console.log('Left arrow clicked!');
+    // Add your event handling code for left arrow here
+});
+
+
+ArrowRight.addEventListener('click', function() {
+    console.log('Right arrow clicked!');
+    // Add your event handling code for right arrow here
+});
