3fc6772852626662dbac87fc26b737539fdd000e
Fabien Costa <fabiencosta94800@gmail.com>
2024-02-02 14:18:54 +0100

    createElement addEventListener on arrow

--- assets/script.js
+++ assets/script.js
@@ -16,3 +16,28 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+
+
+let banner = document.getElementById('banner');
+let arrow = document.createElement('div');
+banner.appendChild(arrow);
+arrow.classList.add('arrow');
+let arrowLeft = document.createElement('img');
+let arrowRight = document.createElement('img');
+// ? faire une fonction pour prendre les deux balise img en un seul ordre
+arrow.appendChild(arrowLeft);
+Object.assign(arrowLeft, {
+	src: "./assets/images/arrow_left.png",
+	alt: 'arrow-left'
+});
+arrow.appendChild(arrowRight);
+Object.assign(arrowRight, {
+	src: "./assets/images/arrow_right.png",
+	alt: 'arrowRight'
+});
+arrowLeft.addEventListener('click', () => {
+	alert("salut bg")
+});
+arrowRight.addEventListener('click', () => {
+	alert("salut fab")
+});
