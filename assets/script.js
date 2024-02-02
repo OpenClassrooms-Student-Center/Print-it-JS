d22eb1935fdbb20b334e087899b3a8ebdae9db5e
Fabien Costa <fabiencosta94800@gmail.com>
2024-02-02 16:34:03 +0100

    dots js

--- assets/script.js
+++ assets/script.js
@@ -17,27 +17,41 @@ const slides = [
 	}
 ]
 
+// ? Arrow part
+function arrowClick () {
+	let banner = document.getElementById('banner');
+	let arrow = document.createElement('div');
+	banner.appendChild(arrow);
+	arrow.classList.add('arrow');
+	let arrowLeft = document.createElement('img');
+	let arrowRight = document.createElement('img');
+	// ? faire une fonction pour prendre les deux balise img en un seul ordre
+	arrow.appendChild(arrowLeft);
+	Object.assign(arrowLeft, {
+		src: "./assets/images/arrow_left.png",
+		alt: 'arrow-left'
+	});
+	arrow.appendChild(arrowRight);
+	Object.assign(arrowRight, {
+		src: "./assets/images/arrow_right.png",
+		alt: 'arrowRight'
+	});
+	arrowLeft.addEventListener('click', () => {
+		alert("gauche")
+	});
+	arrowRight.addEventListener('click', () => {
+		alert("droite")
+	});
+}
+arrowClick();
 
-let banner = document.getElementById('banner');
-let arrow = document.createElement('div');
-banner.appendChild(arrow);
-arrow.classList.add('arrow');
-let arrowLeft = document.createElement('img');
-let arrowRight = document.createElement('img');
-// ? faire une fonction pour prendre les deux balise img en un seul ordre
-arrow.appendChild(arrowLeft);
-Object.assign(arrowLeft, {
-	src: "./assets/images/arrow_left.png",
-	alt: 'arrow-left'
-});
-arrow.appendChild(arrowRight);
-Object.assign(arrowRight, {
-	src: "./assets/images/arrow_right.png",
-	alt: 'arrowRight'
-});
-arrowLeft.addEventListener('click', () => {
-	alert("salut bg")
-});
-arrowRight.addEventListener('click', () => {
-	alert("salut fab")
-});
+// ? Dot part
+let dots = document.querySelector('.dots');
+for (let i = 0 ; i < slides.length ; i++) {
+	let dot = document.createElement('div');
+	dot.classList.add('dot');
+	dots.appendChild(dot);
+	if (i === 0) {
+		dot.classList.add('dot_selected');
+	}
+}
