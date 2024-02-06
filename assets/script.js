f5eea5662c09c074e1335d784acdbb951540a425
Fabien Costa <fabiencosta94800@gmail.com>
2024-02-07 00:50:53 +0100

    add dot in dom

--- assets/script.js
+++ assets/script.js
@@ -48,13 +48,42 @@ function arrowClick() {
 }
 arrowClick();
 // ! =====Dot part=====
+// * Je recupere le container des dots
 let dots = document.querySelector(".dots");
-  for (let i = 0; i < slides.length; i++) {
-    let dot = document.createElement("div");
-    dot.classList.add("dot");
-    dots.appendChild(dot);
-    if (i === 0) {
-      dot.classList.add("dot_selected");
-    }
+// * Je crée une variable pour compter les index
+let indexCounter = 0;
+// * Je crée une fonction pour créer les dots
+function createDot (i) {
+  // * Je crée une variable pour chaque dot
+  let dot = document.createElement("div");
+  // * Je lui donne une class
+  dot.classList.add("dot");
+  // * Je l'ajoute dans le container
+  dots.appendChild(dot);
+  // * Je lui donne une valeur
+  if (i === indexCounter) {
+    // * Je lui donne une class
+    dot.classList.add("dot_selected");
   }
-  
+}
+// * Je crée une fonction pour afficher les dots
+function displayDot () {
+  // * Je crée une boucle pour chaque slide
+  for (let i = 0; i < slides.length; i++) {
+    // * J'appelle la fonction createDot
+    createDot(i);
+  }
+}
+// * J'appelle la fonction displayDot
+ displayDot(); 
+
+
+//  ! premiere tentative de dot
+//   for (let i = 0; i < slides.length; i++) {
+//     let dot = document.createElement("div");
+//     dot.classList.add("dot");
+//     dots.appendChild(dot);
+//     if (i === 0) {
+//       dot.classList.add("dot_selected");
+//     }
+//   }
